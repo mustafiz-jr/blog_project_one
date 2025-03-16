41c02c1fd2b7bc7d74a0cacd9386b382ab0af694
mustafiz-jr <mustafijjr@gmail.com>
2025-03-16 22:44:24 +0600

    a

--- header.php
+++ header.php
@@ -16,43 +16,6 @@
 </head>
 
 <body>
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="" style="  font-family: 'Cookie', cursive;">Mustafizur's Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">User Input</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           Links
-                        </a>
-                        <ul class="dropdown-menu bg-dark text-light" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item text-light" href="#">Login</a></li>
-                            <li><a class="dropdown-item text-light" href="#">sign_up</a></li>
-                        </ul>
-                    </li>
-                   
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2 bg-dark text-secondary" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-info" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-<br> -->
-<!-- <br>
-<hr>
-<br>
-<br> -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
     <div class="container-fluid">
         <a class="navbar-brand" href="" style="font-family: 'Cookie', cursive; font-size: 28px; color:rgb(70, 154, 188);">Mustafizur's Blog</a>
