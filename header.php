4b1e760ea08f0066f33e5ef574c5460594d18f7a
mustafiz-jr <mustafijjr@gmail.com>
2025-03-16 22:43:06 +0600

    some

--- header.php
+++ header.php
@@ -7,9 +7,89 @@
     <title>Blog</title>
     <!--Bootstrap css link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- google font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cookie&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="" style="  font-family: 'Cookie', cursive;">Mustafizur's Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">User Input</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           Links
+                        </a>
+                        <ul class="dropdown-menu bg-dark text-light" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item text-light" href="#">Login</a></li>
+                            <li><a class="dropdown-item text-light" href="#">sign_up</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2 bg-dark text-secondary" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-info" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+<br> -->
+<!-- <br>
+<hr>
+<br>
+<br> -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="" style="font-family: 'Cookie', cursive; font-size: 28px; color:rgb(70, 154, 188);">Mustafizur's Blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#" style="transition: color 0.3s ease-in-out;" onmouseover="this.style.color='rgb(70, 154, 188)'" onmouseout="this.style.color='white'">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" style="transition: color 0.3s ease-in-out;" onmouseover="this.style.color='rgb(70, 154, 188)'" onmouseout="this.style.color='white'">User Input</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="transition: color 0.3s ease-in-out;" onmouseover="this.style.color='rgb(70, 154, 188)'" onmouseout="this.style.color='white'">
+                        Links
+                    </a>
+                    <ul class="dropdown-menu bg-dark text-light" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item text-light" href="#">Login</a></li>
+                        <li><a class="dropdown-item text-light" href="#">Sign Up</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2 bg-dark text-secondary border-light" type="search" placeholder="Search" aria-label="Search" style="border-radius: 20px;">
+                <button class="btn btn-outline-info" type="submit" style="border-radius: 20px; transition: background-color 0.3s ease-in-out;" onmouseover="this.style.backgroundColor='rgb(70, 154, 188)'" onmouseout="this.style.backgroundColor='transparent'">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+
 
 
 
